fbfc5d1842b5caa37671ff69ce6f42df08e4aab6
Ian36 <ianhuang7991@gmail.com>
2021-09-12 17:20:28 +1000

    endpoint fix

--- Controllers/SvfController.cs
+++ Controllers/SvfController.cs
@@ -27,11 +27,11 @@ namespace CapstoneProject.Controllers
             _logger = logger;
         }
 
-	[HttpGet]
-	public async Task<IActionResult> Get([FromQuery] string input)
-	{
-	    return Ok(input);
-	}
+        [HttpGet]
+        public IActionResult Get([FromQuery] string input)
+        {
+            return Ok(input);
+        }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] RequestBody requestBody)
